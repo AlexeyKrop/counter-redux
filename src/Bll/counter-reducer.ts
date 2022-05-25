bcf8dc16356d229953a7e6e5a1e33698acde8d00
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-25 09:40:13 +0300

    add IncrementCountAC and IncrementCountAT

--- src/Bll/counter-reducer.ts
+++ src/Bll/counter-reducer.ts
@@ -1,14 +1,20 @@
 let initialState = {
   startValue: 0,
   maxValue: 10,
+  count: 0
 }
 
 type InitialStateType = typeof initialState
 export const counterReducer = (state:InitialStateType =  initialState, action: CounterReducerAT):InitialStateType => {
+
  switch (action.type) {
    case "SET_VALUES":
      return{
        ...state, startValue: action.startValue, maxValue: action.maxValue
+     }
+   case "INC_COUNT":
+     return{
+       ...state, count: action.count + 1
      }
    default:
     return state
@@ -18,5 +24,7 @@ export const counterReducer = (state:InitialStateType =  initialState, action: C
 
 
 export const SetValuesForSetDisplayAC = (startValue: number, maxValue: number) => ({type: 'SET_VALUES', startValue: startValue, maxValue: maxValue} as const)
-type CounterReducerAT = SetValuesForSetDisplayAT
+export const IncrementCountAC = (count: number) => ({type: 'INC_COUNT', count: count} as const)
+type CounterReducerAT = SetValuesForSetDisplayAT | IncrementCountAT
 type SetValuesForSetDisplayAT = ReturnType<typeof SetValuesForSetDisplayAC>
+type IncrementCountAT = ReturnType<typeof IncrementCountAC>
