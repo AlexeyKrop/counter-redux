a7bae8b25f2b056a117f0742dc4c15f9a619a22c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-25 09:53:36 +0300

    add setDisabled

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   let [disabled, setDisabled] = useState(true)
   const dispatch = useDispatch()
   const setValueInSetDisplay = (startValue: number, maxValue: number) => {
+    setDisabled(false)
     dispatch(SetValuesForSetDisplayAC(startValue, maxValue))
   }
   const getCounterIncrement = () => {
@@ -28,8 +29,8 @@ function App() {
         <div className="wrapper-counter">
           <Display count={count} maxCount={maxValue} startCount = {startValue}  error={''}/>
           <div className="button__group">
-            <Button callBack={getCounterIncrement} name={'Inc'}/>
-            <Button callBack={getResetICount} name={'Reset'}/>
+            <Button disabled={disabled} callBack={getCounterIncrement} name={'Inc'}/>
+            <Button disabled={disabled} callBack={getResetICount} name={'Reset'}/>
           </div>
         </div>
         <div className="wrapper-counter wrapper-counter__setting">
