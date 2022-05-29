8acd3aed24d99414f124aa0e9e7ee816a1cc4495
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-29 16:00:11 +0300

    add check set value in display

--- src/Bll/counter-reducer.test.ts
+++ src/Bll/counter-reducer.test.ts
@@ -1,4 +1,10 @@
-import {counterReducer, IncrementCountAC, InitialStateType, ResetCountAC} from "./counter-reducer";
+import {
+  counterReducer,
+  IncrementCountAC,
+  InitialStateType,
+  ResetCountAC,
+  SetValuesForSetDisplayAC
+} from "./counter-reducer";
 
 test('check increment count', () => {
 
@@ -28,5 +34,20 @@ test('check reset count', () => {
     count: 0,
     maxValue: 10,
     startValue: 0,
+  })
+})
+test('check set value in display', () => {
+
+  const startState: InitialStateType = {
+    startValue: 0,
+    maxValue: 10,
+    count: 10
+  }
+  const action = SetValuesForSetDisplayAC(2, 99)
+  const endState = counterReducer(startState, action)
+  expect(endState).toStrictEqual({
+    count: 2,
+    maxValue: 99,
+    startValue: 2,
   })
 })
